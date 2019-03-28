2c2a08598ae72aefeeb24675a462d68c93363c2a
h.c.ward1991@gmail.com <h.c.ward1991@gmail.com>
2019-03-28 16:21:49 -0500

    Add project files.

---
+++ CarClass/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CarClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+
+    class Car
+    {
+        public float gasLevel { get; set; }
+
+
+        static Car()
+        {
+            float startingGasLevel = new Random().Next(1, 13);
+        }
+
+        public float AddGas(float startingGasLevel)
+        {
+            float newGasLevel = gasLevel + startingGasLevel;
+
+            return newGasLevel;
+        }
+
+        public static float FillUp(float newGasLevel)
+        {
+            float amountOfGasAddedToFillUp = 13 - newGasLevel;
+
+            return amountOfGasAddedToFillUp;
+        }
+
+    }
+}
